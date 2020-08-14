c72a10cb555708085e23894f3bdb671375af5974
Karina-Kosenko <karina.kosenko@techs.com.ua>
2020-08-14 17:39:07 +0300

    Section-18-point-274

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,16 +1,9 @@
-import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {tap} from 'rxjs/operators';
+import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request');
     const modifiedRequest = request.clone({headers: request.headers.append('Auth', 'xyz')});
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived');
-        console.log(event.body);
-      }
-    }));
+    return next.handle(modifiedRequest);
   }
 }
 
---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,15 @@
+import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    console.log('Outgoing request ' + request.url);
+    return next.handle(request).pipe(tap(event => {
+      if (event.type === HttpEventType.Response) {
+        console.log('Incoming response');
+        console.log(event.body);
+      }
+    }));
+  }
+}
+
