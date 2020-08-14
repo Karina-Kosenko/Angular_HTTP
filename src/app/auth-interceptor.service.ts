5f2dd3ac06b2370574272ce9ba249318519b4520
Karina-Kosenko <karina.kosenko@techs.com.ua>
2020-08-14 17:28:32 +0300

    Section-18-point-272

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,7 +3,8 @@ import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler) {
     console.log('Request');
-    return next.handle(request);
+    const modifiedRequest = request.clone({headers: request.headers.append('Auth', 'xyz')});
+    return next.handle(modifiedRequest);
   }
 }
 
